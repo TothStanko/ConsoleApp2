82b4db4b82e32137ff617a3991f45f96e035262a
b19TóthS <b19TthS@204-12>
2023-10-02 12:49:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "Stanko";
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
