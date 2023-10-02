dcc4e403b3acc55f32ef559338b8332c0864b38a
Stanko <tothstanko@gmail.com>
2023-10-02 13:27:16 +0200

    add cat handling

--- Program.cs
+++ Program.cs
@@ -8,6 +8,18 @@ namespace ConsoleApp2
         {
             string name = "Stanko";
             Console.WriteLine($"Hello {name}!");
+            Console.WriteLine($"{name}, do you like cats?");
+            Console.Write(">>>");
+            string resp = Console.ReadLine();
+            if (resp.ToLower().StartsWith("yes"))
+            {
+                Console.WriteLine("awwwww :3 :3 :3");
+            }
+            else
+            {
+                Console.WriteLine(":((((((");
+            }
+
         }
     }
 }
