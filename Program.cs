5d5d920999299ccad89ed5768e852cf8fc04046b
Stanko <tothstanko@gmail.com>
2023-10-02 13:44:25 +0200

    add cat

--- Program.cs
+++ Program.cs
@@ -6,13 +6,39 @@ namespace ConsoleApp2
     {
         static void Main()
         {
-            string name = "Stanko";
-            Console.WriteLine($"Hello {name}!");
+            static void Main()
+            {
+                string name = "Zoli";
+                Console.WriteLine($"Hello, {name}!");
+                Console.WriteLine($"{name}, do you like cats?");
+                Console.Write(">>>  ");
+                string resp = Console.ReadLine();
+                if (resp.ToLower().StartsWith("yes"))
+                {
+                    Console.WriteLine("awwwww :3 :3 :3");
+                }
+                else Console.WriteLine(":(((((");
 
-            Console.WriteLine("Do you like cats?");
-            Console.WriteLine(">>>");
+                Console.WriteLine("What is your favorite color?");
+                Console.Write(">>>  ");
+                resp = Console.ReadLine();
+                if (resp.ToLower().Contains("blue"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("This?");
+                }
+                else if (resp.ToLower().Contains("red"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This?");
+                }
+                else if (resp.ToLower().Contains("green"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("This?");
+                }
+                Console.ReadKey(true);
 
-
-        }
+            }
     }
 }
